59d1f32efadb1ae0a8bf85e28bc03031c172d8d3
sreevidyaraman <sreevidyaraman92@gmail.com>
2024-10-01 14:30:44 +0530

    Add files via upload

--- text2api.py
+++ text2api.py
@@ -2,11 +2,12 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame({
-    'Title': ['User Story', 'Priority', 'Uploaded', 'Valide', 'Invalid', 'Incomplete', 'Variants', 'Typo', 'Total'],
-    'Row1': [0, 0, 0],
-    'Row2': [0, 0, 0]
+    'Title': ['User Story', 'Priority'],
+    'SN1': [0, 0, 0],
+    'SN2': [0, 0, 0]
 })
 
+#    'Title': ['User Story', 'Priority', 'Uploaded', 'Valide', 'Invalid', 'Incomplete', 'Variants', 'Typo', 'Total'],
 
 number = st.slider("Pick a number", 0, 100)
 st.write("Values:", number)
@@ -16,8 +17,8 @@ input_A = st.number_input('User Story 1', value=0.0)
 input_B = st.number_input('User Story 2', value=0.0)
 
 # Update dataframe with user input
-df.at[0, 'User Story'] = input_A
-df.at[1, 'User Story'] = input_B
+df.at[0, 'SN1'] = input_A
+df.at[1, 'SN2'] = input_B
 
 # Display the updated dataframe
 st.table(df.set_index('Title'))
