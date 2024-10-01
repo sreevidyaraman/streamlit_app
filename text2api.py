78bfaf73447405ab0df3875d9f7e2e45d1fcf98e
sreevidyaraman <sreevidyaraman92@gmail.com>
2024-10-01 22:39:14 +0530

    Add files via upload

--- text2api.py
+++ text2api.py
@@ -6,10 +6,26 @@ number = st.slider("Pick a number", 0, 100)
 st.write("Values:", number)
 
 
-
 df = pd.DataFrame({
-    'Title': ['User Story', 'Priority'],
-    'SN1': [0, 0, 0],
-    'SN2': [0, 0, 0]
+    'Title': ['A', 'B', 'Total'],
+    'Row1': [0, 0, 0],
+    'Row2': [0, 0, 0]
 })
 
+
+# Getting user input
+input_A = st.number_input('Input for A', value=0.0)
+input_B = st.number_input('Input for B', value=0.0)
+
+# Update dataframe with user input
+df.at[0, 'Row1'] = input_A
+df.at[1, 'Row1'] = input_B
+df.at[0, 'Row2'] = input_A
+df.at[1, 'Row2'] = input_B
+
+# Recalculate the Total column
+df.at[2, 'Row1'] = df['Row1'].sum()
+df.at[2, 'Row2'] = df['Row2'].sum()
+
+# Display the updated dataframe
+st.table(df.set_index('Title'))
